e1da61fb4ac0874812b47031ef6e9e86a80226ae
Luis Adorno <luis.adorno5@upr.edu>
2022-03-16 22:51:07 -0400

    Add CreateView and Create functionality in the controller.

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,14 +1,46 @@
 ﻿using System;
+using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BulkyBookWeb.Controllers
+namespace BulkyBookWeb.Controllers;
+
+public class CategoryController : Controller
 {
-	public class CategoryController : Controller
-	{
-		public IActionResult Index()
+    private readonly ApplicationDbContext _db;
+
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+	public IActionResult Index()
+    {
+        IEnumerable<Category> objCategoryList = _db.Categories;
+		return View(objCategoryList);
+    }
+
+    //GET
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Category obj)
+    {
+        if (ModelState.IsValid)
         {
-			return View();
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
-	}
+        else
+            return View(obj);
+        
+    }
 }
+
 
