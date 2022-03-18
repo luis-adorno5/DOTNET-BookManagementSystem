f32a9f3cc4151bb5420dd98cffeb08d745e97be4
Luis Adorno <luis.adorno5@upr.edu>
2022-03-18 19:49:35 -0400

    Added client side validation.

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
@@ -11,6 +12,8 @@ namespace BulkyBookWeb.Models
         [Required]
         public string Name { get; set; }
 
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display order must be between 1 and 100 only!!!")]
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
