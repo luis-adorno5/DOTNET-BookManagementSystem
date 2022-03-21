79756252cad89a9409f557429b1b258be087187f
Luis Adorno <luis.adorno5@upr.edu>
2022-03-21 18:05:41 -0400

    Add delete functiona

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -45,6 +45,56 @@ public class CategoryController : Controller
             return View(obj);
         
     }
+
+    //GET
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0) return NotFound();
+        var categoryFromDb = _db.Categories.Find(id);
+        if (categoryFromDb == null) return NotFound();
+        return View(categoryFromDb);
+    }
+
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("name", "DisplayOrder cannot exactly match the name!");
+        }
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        else
+            return View(obj);
+
+    }
+
+    //GET
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0) return NotFound();
+        var categoryFromDb = _db.Categories.Find(id);
+        if (categoryFromDb == null) return NotFound();
+        return View(categoryFromDb);
+    }
+
+    //POST
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePOST(int? id)
+    {
+        var category = _db.Categories.Find(id);
+        if (category == null) return NotFound();
+        _db.Remove(category);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
 }
 
 
